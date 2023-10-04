a7cf49441a56cf1614c83aa7f21aaf47a6495efd
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-10-04 08:51:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('The\nZero\nBot', {
+say('The\nAnnie\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por Gabi`, {
+say(`by @designs.annie`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
