61ec7d39e826fda65cf52b89dca799004df4c8b0
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-10-04 19:41:48 +0200

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'TheZero', body: '@designs.annie', sourceUrl: `https://www.instagram.com/designs.annie`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: 'Annie', body: '@designs.annie', sourceUrl: `https://www.instagram.com/designs.annie`, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
